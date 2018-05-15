d24defa92931226fb13f60352848e9240fe60b67
GkHabib <h.ghadimi1998@gmail.com>
2018-05-15 23:59:54 +0430

    Fix Comma

--- Datapath.v
+++ Datapath.v
@@ -1,13 +1,13 @@
 module Datapath (clk, rst, pcInc, reg1Or2, PcOrTR, regOrMem, RegBOr0, RegAOr0, DiToCU, IrToCU,
-    CznToCU, pcLoadEn,);
+    CznToCU, pcLoadEn, diLoadEn);
 
-    input clk, rst, reg1Or2, PcOrTR, regOrMem, RegBOr0, RegAOr0, pcLoadEn;
+    input clk, rst, reg1Or2, PcOrTR, regOrMem, RegBOr0, RegAOr0, pcLoadEn, diLoadEn;
     output [4:0] DiToCU;
     output [3:0] IrToCU;
     output [2:0] CznToCU;
     wire [12:0] pcOut, trOut;
     PC PC_ (.clk(clk), .rst(rst), .inData(trOut), .outData(pcOut), .inc(pcInc), .loadEn(pcLoadEn));
-    DI DI_ (clk, rst, ld, in, out);
+    DI DI_ (.clk(clk), .rst(rst), .ld(diLoadEn), .in, out);
     TwoTwoBitInputMUX AccumulatorAddressMUX_ (a, b, sel, out);
     Accumulator Accumulator_ (clk, rst, inData, address, outData, writeEn);
     TwoThirteenBitInputMUX MemoryAddressMUX_ (a, b, sel, out);
