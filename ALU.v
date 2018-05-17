482837ec08c1fdfee5852a1bd880be5a84dd139b
GkHabib <h.ghadimi1998@gmail.com>
2018-05-18 01:59:32 +0430

    fix czn port declaration

--- ALU.v
+++ ALU.v
@@ -4,18 +4,18 @@ module ALU (a, b, opControl, c, result, czn);
   input[1:0] opControl;
   input c;
   output reg[7:0] result;
-  output reg[2:0] czn;
+  output [2:0] czn;
   parameter [1:0] ADD = 2'b00, AND = 2'b01, OR = 2'b10;
 
   always @ ( a, b, c, opControl ) begin
     case(opControl)
-    ADD: {czn[2], result} <= a + b + c;
+    ADD: {czn[0], result} <= a + b + c;
     AND: result <= a & b;
     OR: result <= a | b;
     endcase
   end
 
   assign czn[1] = (result == 0);
-  assign czn[0] = (result < 0);
+  assign czn[2] = (result < 0);
 
 endmodule // ALU
