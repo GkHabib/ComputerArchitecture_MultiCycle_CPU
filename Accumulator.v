b3ca35bda7698398a6a10d25ba382e3a0cd1065d
GkHabib <h.ghadimi1998@gmail.com>
2018-05-18 02:00:42 +0430

    fix output port declaration

--- Accumulator.v
+++ Accumulator.v
@@ -1,9 +1,9 @@
 module Accumulator (clk, rst, inData, address, outData, writeEn);
 
   input clk, rst, writeEn;
-  input[7:0] inData;
-  input[12:0] address;
-  output reg[7:0] outData;
+  input [7:0] inData;
+  input [12:0] address;
+  output [7:0] outData;
 
   reg[7:0] regArr [3:0];
 
