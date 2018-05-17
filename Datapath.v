1bddee3801207c7914037d5aa71a9ce5d98c0e52
GkHabib <h.ghadimi1998@gmail.com>
2018-05-18 01:47:56 +0430

    Datapath done

--- Datapath.v
+++ Datapath.v
@@ -1,27 +1,34 @@
 module Datapath (clk, rst, pcInc, reg1Or2, PcOrTR, regOrMem, RegBOr0, RegAOr0, DiToCU, IrToCU,
-    CznToCU, pcLoadEn, diLoadEn);
+    CznToCU, pcLoadEn, diLoadEn, accumulatorWriteEn, memoryReadEn, memoryWriteEn,
+    irWriteEn, trWriteEn, bRegWriteEn, aRegWriteEn, aluOpControl, aluResWriteEn, ldCZN);
 
-    input clk, rst, reg1Or2, PcOrTR, regOrMem, RegBOr0, RegAOr0, pcLoadEn, diLoadEn;
-    output [4:0] DiToCU;
+    input clk, rst, reg1Or2, PcOrTR, regOrMem, RegBOr0, RegAOr0, pcLoadEn, diLoadEn, accumulatorWriteEn,
+      memoryReadEn, memoryWriteEn, irWriteEn, trWriteEn, bRegWriteEn, aRegWriteEn, aluResWriteEn, ldCZN;
+    input [1:0] aluOpControl;
+    output [4:0] diToCU;
     output [3:0] IrToCU;
     output [2:0] CznToCU;
-    wire [12:0] pcOut, trOut;
+    wire [12:0] pcOut, trOut, memAddr;
+    wire [7:0] irOut, aluResOut, accumulatorOut, memoryOut, bRegInput, bRegOutput, aRegOutput, aluIn1, aluIn2, aluOut;
+    wire [4:0] diOut;
+    wire [2:0] cznOut, cznIn;
+    wire [1:0] accumulatorAddr;
     PC PC_ (.clk(clk), .rst(rst), .inData(trOut), .outData(pcOut), .inc(pcInc), .loadEn(pcLoadEn));
-    DI DI_ (.clk(clk), .rst(rst), .ld(diLoadEn), .in, out);
-    TwoTwoBitInputMUX AccumulatorAddressMUX_ (a, b, sel, out);
-    Accumulator Accumulator_ (clk, rst, inData, address, outData, writeEn);
-    TwoThirteenBitInputMUX MemoryAddressMUX_ (a, b, sel, out);
-    Memory Memory_ (clk, rst, inData, address, outData, readEn, writeEn);
-    EightBitReg IR_ (clk, rst, writeEn, in, out);
-    TR TR_ (clk, rst, ld, in, out);
-    TwoEightBitInputMUX BRegInputMUX_ (a, b, sel, out);
-    EightBitReg BReg_ (clk, rst, writeEn, in, out);
-    EightBitReg AReg_ (clk, rst, writeEn, in, out);
-    TwoEightBitInputMUX BRegToALuMUX_ (a, b, sel, out);
-    TwoEightBitInputMUX ARegToALuMUX_ (a, b, sel, out);
-    ALU ALU_ (a, b, opControl, c, result, czn);
-    EightBitReg ALUResult_ (clk, rst, writeEn, in, out);
-    CZN CZN_ (clk, rst, ld, in, out);
+    DI DI_ (.clk(clk), .rst(rst), .ld(diLoadEn), .in(irOut[4:0]), .out(diOut));
+    TwoTwoBitInputMUX AccumulatorAddressMUX_ (.a(diOut[1:0]), .b(diOut[3:2]), .sel(reg1Or2), .out(accumulatorAddr));
+    Accumulator Accumulator_ (.clk(clk), .rst(rst), .inData(aluResOut), .address(accumulatorAddr), .outData(accumulatorOut), .writeEn(accumulatorWriteEn));
+    TwoThirteenBitInputMUX MemoryAddressMUX_ (.a(trOut), .b(pcOut), .sel(PcOrTR), .out(memAddr));
+    Memory Memory_ (.clk(clk), .rst(rst), .inData(aluResOut), .address(memAddr), .outData(memoryOut), .readEn(memoryReadEn), .writeEn(memoryWriteEn));
+    EightBitReg IR_ (.clk(clk), .rst(rst), .writeEn(irWriteEn), .in(memoryOut), .out(irOut));
+    TR TR_ (.clk(clk), .rst(rst), .ld(trWriteEn), .in({irOut[4:0], memoryOut}), .out(trOut));
+    TwoEightBitInputMUX BRegInputMUX_ (.a(memoryOut), .b(accumulatorOut), .sel(regOrMem), .out(bRegInput));
+    EightBitReg BReg_ (.clk(clk), .rst(rst), .writeEn(bRegWriteEn), .in(bRegInput), .out(bRegOutput));
+    EightBitReg AReg_ (.clk(clk), .rst(rst), .writeEn(aRegWriteEn), .in(accumulatorOut), .out(aRegOutput));
+    TwoEightBitInputMUX BRegToALuMUX_ (.a(bRegOutput), .b(8'b0), .sel(RegBOr0), .out(aluIn1));
+    TwoEightBitInputMUX ARegToALuMUX_ (.a(aRegOutput), .b(8'b0), .sel(RegAOr0), .out(aluIn2));
+    ALU ALU_ (.a(aluIn1), .b(aluIn2), .opControl(aluOpControl), .c(cznOut[0]), .result(aluOut), .czn(cznIn));
+    EightBitReg ALUResult_ (.clk(clk), .rst(rst), .writeEn(aluResWriteEn), .in(aluOut), .out(aluResOut));
+    CZN CZN_ (.clk(clk), .rst(rst), .ld(ldCZN), .in(cznIn), .out(cznOut));
 
 
 endmodule // Datapath
