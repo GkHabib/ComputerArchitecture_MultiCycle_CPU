f78b8bf188cbfc686e504eab3a8ce8afec1dc488
GkHabib <h.ghadimi1998@gmail.com>
2018-05-18 18:00:04 +0430

    change module

--- ThreeTwoBitInputMUX.v
+++ ThreeTwoBitInputMUX.v
@@ -1,7 +1,13 @@
-module ThreeTwoBitInputMUX (in, sel, out);
-  input [1:0] in [2:0]
-  input[1:0]  sel;
-  output[1:0] out;
-  assign out = in[sel];
+module ThreeTwoBitInputMUX (in1, in2, in3, sel, out);
+  input[1:0] in1, in2, in3, sel;
+  output reg [1:0] out;
+  always @ (in1,in2,in3,sel) begin
+    case(sel)
+      0: out = in1;
+      1: out = in2;
+      2: out = in3;
+      default: out = 2'b0;
+    endcase
+  end
 
 endmodule // ThreeTwoBitInputMUX
