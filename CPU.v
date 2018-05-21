f4141939777fe07114fd96102eaa68b519e32a31
GkHabib <h.ghadimi1998@gmail.com>
2018-05-21 18:32:09 +0430

    Add "CC" signal tu CPU

--- CPU.v
+++ CPU.v
@@ -5,13 +5,13 @@ module CPU (clk, rst, start, done);
   wire [3:0] IrToCU;
   wire [2:0] CznToCU;
   wire pcInc, PcOrTR, regOrMem, RegBOr0, RegAOr0, pcLoadEn, diLoadEn, accumulatorWriteEn,
-    memoryWriteEn, irWriteEn, trWriteEn, bRegWriteEn, aRegWriteEn, aluResWriteEn, ldCZN;
+    memoryWriteEn, irWriteEn, trWriteEn, bRegWriteEn, aRegWriteEn, aluResWriteEn, ldCZN, CC;
   wire [1:0] aluOpControl, accAddressSel;
 
   Datapath DP(.clk(clk), .rst(rst), .pcInc(pcInc), .accAddressSel(accAddressSel), .PcOrTR(PcOrTR), .regOrMem(regOrMem), .RegBOr0(RegBOr0), .RegAOr0(RegAOr0), .DiToCU(DiToCU), .IrToCU(IrToCU),
       .CznToCU(CznToCU), .pcLoadEn(pcLoadEn), .diLoadEn(diLoadEn), .accumulatorWriteEn(accumulatorWriteEn), .memoryWriteEn(memoryWriteEn),
-      .irWriteEn(irWriteEn), .trWriteEn(trWriteEn), .bRegWriteEn(bRegWriteEn), .aRegWriteEn(aRegWriteEn), .aluOpControl(aluOpControl), .aluResWriteEn(aluResWriteEn), .ldCZN(ldCZN));
+      .irWriteEn(irWriteEn), .trWriteEn(trWriteEn), .bRegWriteEn(bRegWriteEn), .aRegWriteEn(aRegWriteEn), .aluOpControl(aluOpControl), .aluResWriteEn(aluResWriteEn), .ldCZN(ldCZN), .CC(CC));
   Controller CU(.clk(clk), .rst(rst), .start(start), .pcInc(pcInc), .done(done), .accAddressSel(accAddressSel), .PcOrTR(PcOrTR), .regOrMem(regOrMem), .RegBOr0(RegBOr0), .RegAOr0(RegAOr0), .DiToCU(DiToCU), .IrToCU(IrToCU),
           .CznToCU(CznToCU), .pcLoadEn(pcLoadEn), .diLoadEn(diLoadEn), .accumulatorWriteEn(accumulatorWriteEn), .memoryWriteEn(memoryWriteEn),
-          .irWriteEn(irWriteEn), .trWriteEn(trWriteEn), .bRegWriteEn(bRegWriteEn), .aRegWriteEn(aRegWriteEn), .aluOpControl(aluOpControl), .aluResWriteEn(aluResWriteEn), .ldCZN(ldCZN));
+          .irWriteEn(irWriteEn), .trWriteEn(trWriteEn), .bRegWriteEn(bRegWriteEn), .aRegWriteEn(aRegWriteEn), .aluOpControl(aluOpControl), .aluResWriteEn(aluResWriteEn), .ldCZN(ldCZN), .CC(CC));
 endmodule // CPU
